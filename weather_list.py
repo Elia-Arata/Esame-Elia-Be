ca283d9f7e1cca60754781203d045937a0f4ef7b
Elia Arata <elia93.arata@gmail.com>
2024-04-29 18:25:52 +0200

    fix

--- weather_list.py
+++ weather_list.py
@@ -5,118 +5,118 @@ weather = [
         "temperature": -4.00,
         "pressure": 0.950,
         "humidity": 1.0,
-        "dew_point": -4.00
+        "dew_point": -5.00
     },
     {
-        "id" : 1,
-        "day": "28 MAR 2022",
-        "temperature": -4.00,
-        "pressure": 0.950,
+        "id" : 2,
+        "day": "29 MAR 2022",
+        "temperature": -3.00,
+        "pressure": 0.850,
         "humidity": 0.5,
         "dew_point": -4.00
     },
     {
-        "id" : 1,
-        "day": "28 MAR 2022",
-        "temperature": -4.00,
-        "pressure": 0.950,
+        "id" : 3,
+        "day": "26 MAR 2022",
+        "temperature": -24.00,
+        "pressure": 0.750,
         "humidity": 0.4,
-        "dew_point": -4.00
+        "dew_point": -6.00
     },
     {
-        "id" : 1,
-        "day": "28 MAR 2022",
-        "temperature": -4.00,
-        "pressure": 0.950,
+        "id" : 4,
+        "day": "28 APR 2022",
+        "temperature": 4.00,
+        "pressure": 0.360,
         "humidity": 0.5,
-        "dew_point": -4.00
+        "dew_point": -7.00
     },
     {
-        "id" : 1,
-        "day": "28 MAR 2022",
-        "temperature": -4.00,
-        "pressure": 0.950,
+        "id" : 5,
+        "day": "25 MAR 2022",
+        "temperature": -5.00,
+        "pressure": 0.790,
+        "humidity": 0.2,
+        "dew_point": -8.00
+    },
+    {
+        "id" : 6,
+        "day": "24 MAR 2022",
+        "temperature": -6.00,
+        "pressure": 0.670,
         "humidity": 0.2,
         "dew_point": -4.00
     },
     {
-        "id" : 1,
-        "day": "28 MAR 2022",
-        "temperature": -4.00,
-        "pressure": 0.950,
-        "humidity": 0.2,
-        "dew_point": -4.00
-    },
-    {
-        "id" : 1,
-        "day": "28 MAR 2022",
-        "temperature": -4.00,
-        "pressure": 0.950,
+        "id" : 7,
+        "day": "23 MAR 2022",
+        "temperature": -7.00,
+        "pressure": 0.450,
         "humidity": 0.3,
-        "dew_point": -4.00
+        "dew_point": -1.00
     },
     {
-        "id" : 1,
-        "day": "28 MAR 2022",
-        "temperature": -4.00,
-        "pressure": 0.950,
+        "id" : 8,
+        "day": "22 MAR 2022",
+        "temperature": 8.00,
+        "pressure": 0.870,
         "humidity": 0.8,
-        "dew_point": -4.00
+        "dew_point": -4.50
     },
     {
-        "id" : 1,
-        "day": "28 MAR 2022",
-        "temperature": -4.00,
-        "pressure": 0.950,
-        "humidity": 1.0,
-        "dew_point": -4.00
+        "id" : 9,
+        "day": "21 MAR 2022",
+        "temperature": 9.00,
+        "pressure": 0.380,
+        "humidity": 2.5,
+        "dew_point": -2.00
     },
     {
-        "id" : 1,
-        "day": "28 MAR 2022",
-        "temperature": -4.00,
-        "pressure": 0.950,
+        "id" : 10,
+        "day": "20 MAR 2022",
+        "temperature": 10.00,
+        "pressure": 0.550,
         "humidity": 1.0,
-        "dew_point": -4.00
+        "dew_point": -3.00
     },
     {
-        "id" : 1,
-        "day": "28 MAR 2022",
-        "temperature": -4.00,
-        "pressure": 0.950,
-        "humidity": 1.0,
-        "dew_point": -4.00
+        "id" : 11,
+        "day": "19 MAR 2022",
+        "temperature": 11.00,
+        "pressure": 0.150,
+        "humidity": 5.0,
+        "dew_point": 4.00
     },
     {
-        "id" : 1,
-        "day": "28 MAR 2022",
-        "temperature": -4.00,
-        "pressure": 0.950,
-        "humidity": 1.0,
-        "dew_point": -4.00
+        "id" : 12,
+        "day": "18 MAR 2022",
+        "temperature": 12.00,
+        "pressure": 0.350,
+        "humidity": 4.0,
+        "dew_point": 3.00
     },
     {
-        "id" : 1,
-        "day": "28 MAR 2022",
-        "temperature": -4.00,
-        "pressure": 0.950,
-        "humidity": 1.0,
-        "dew_point": -4.00
+        "id" : 13,
+        "day": "17 MAR 2022",
+        "temperature": 13.00,
+        "pressure": 0.850,
+        "humidity": 1.5,
+        "dew_point": 5.00
     },
     {
-        "id" : 1,
-        "day": "28 MAR 2022",
+        "id" : 14,
+        "day": "16 MAR 2022",
         "temperature": -4.00,
-        "pressure": 0.950,
-        "humidity": 1.0,
-        "dew_point": -4.00
+        "pressure": 0.450,
+        "humidity": 3.0,
+        "dew_point": 4.00
     },
     {
-        "id" : 1,
-        "day": "28 MAR 2022",
+        "id" : 15,
+        "day": "15 MAR 2022",
         "temperature": -4.00,
-        "pressure": 0.950,
-        "humidity": 1.0,
-        "dew_point": -4.00
+        "pressure": 0.970,
+        "humidity": 2.0,
+        "dew_point": 1.00
     }
 ]
